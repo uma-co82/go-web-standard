bb8454c26970746c4a37404662788c316a6c6927
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-10-29 22:41:45 +0900

    [update] http/2

--- src/main.go
+++ src/main.go
@@ -1,43 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"text/template"
+	"reflect"
+	"runtime"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	files := http.FileServer(http.Dir("/public"))
-	mux.Handle("/static/", http.StripPrefix("/static/", files))
-
-	mux.HandleFunc("/", index)
-
-	server := &http.Server{
-		Addr:    "0.0.0.0:8080",
-		Handler: mux,
-	}
-
-	server.ListenAndServe()
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	threads, err := data.Threads()
-	if err == nil {
-		_, err := session(w, r)
-		public_tmpl_files := []string{"templates/layout.html",
-			"templates/public.navbar.html",
-			"templates/index.html",
-		}
-		private_tmpl_files := []string{"templates/layout.html",
-			"templates/private.navbar.html",
-			"templates/index.html",
-		}
-		var templates *template.Template
-		if err != nil {
-			templates = template.Must(template.ParseFiles(private_tmpl_files...))
-		} else {
-			templates = template.Must(template.ParseFiles(public_tmpl_files...))
-		}
-		templates.ExecuteTemplate(w, "layout", threads)
+func world(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "World!")
+}
+
+func log(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+		fmt.Println("Handler function called - " + name)
+		h(w, r)
 	}
+}
+
+func main() {
+
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+
+	http.HandleFunc("/hello", log(hello))
+	http.HandleFunc("/world", world)
+
+	server.ListenAndServe()
 }
