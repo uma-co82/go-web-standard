2e6ea0068543e70739e43da4d6f295622de44105
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-10-30 17:39:21 +0900

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"reflect"
 	"runtime"
 )
+
+type Post struct {
+	User    string
+	Threads []string
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
@@ -23,6 +29,57 @@ func log(h http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+func headers(w http.ResponseWriter, r *http.Request) {
+	h := r.Header
+	fmt.Fprintln(w, h)
+}
+
+func writeExample(w http.ResponseWriter, r *http.Request) {
+	str := `<html>
+	<head><title>Go Web Programming</title></head>
+	<body><h1>Hello World</h1></body>
+	</html>`
+	w.Write([]byte(str))
+}
+
+func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(501)
+	fmt.Fprintln(w, "501")
+}
+
+func headerExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Location", "http://google.com")
+	w.WriteHeader(302)
+}
+
+func jsonExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	post := &Post{
+		User:    "Sau Sheong",
+		Threads: []string{"1番目", "2番目", "3番目"},
+	}
+	json, _ := json.Marshal(post)
+	w.Write(json)
+}
+
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	c1 := http.Cookie{
+		Name:     "first_cookie",
+		Value:    "Go Web Programming",
+		HttpOnly: true,
+	}
+
+	c2 := http.Cookie{
+		Name:     "second_cookie",
+		Value:    "Manning Publications Co",
+		HttpOnly: true,
+	}
+
+	fmt.Fprintln(w, "hoge")
+	w.Header().Set("Set-Cookie", c1.String())
+	w.Header().Add("Set-Cookie", c2.String())
+}
+
 func main() {
 
 	server := http.Server{
@@ -31,6 +88,12 @@ func main() {
 
 	http.HandleFunc("/hello", log(hello))
 	http.HandleFunc("/world", world)
+	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/write", writeExample)
+	http.HandleFunc("/writeheader", writeHeaderExample)
+	http.HandleFunc("/redirect", headerExample)
+	http.HandleFunc("/json", jsonExample)
+	http.HandleFunc("/set_cookie", setCookie)
 
 	server.ListenAndServe()
 }
